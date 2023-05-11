5e7fa02d36e07c7d6503dde23246ffd6b2480b68
hex4coder <the.programmer.luyo@gmail.com>
2023-05-11 22:00:24 +0800

    Penambahan grafik data perkara

--- sptsp/app/Http/Controllers/HomeController.php
+++ sptsp/app/Http/Controllers/HomeController.php
@@ -27,11 +27,35 @@ class HomeController extends Controller
     public function index()
     {
 
+        $dataGrafik = [];
+        $totalDataPerkara  = 0;
+        $jenisPerkaras = JenisPerkara::all();
+        $listJenis = [];
+
+        foreach ($jenisPerkaras as $jp) {
+            array_push($listJenis, $jp->jenis);
+            $jenis_perkara_id = $jp->id;
+            $perkaras = Perkara::where('jenis_perkara_id', $jenis_perkara_id)->get();
+            array_push($dataGrafik, [
+                'jenis' => $jp,
+                'perkara' => count($perkaras),
+        
+            ]);
+            $totalDataPerkara += count($perkaras);
+        }
+
+
         $jumlahData = [
             'jenis_perkara' => JenisPerkara::count(),
             'prasyarat' => Prasyarat::count(),
             'perkara' => Perkara::count(),
+            'grafik' => [
+                'total' => $totalDataPerkara,
+                'data' => $dataGrafik,
+                'list_jenis' => $listJenis,
+            ],
         ];
+
 
 
         return view('home', [
--- sptsp/resources/views/home.blade.php
+++ sptsp/resources/views/home.blade.php
@@ -3,57 +3,125 @@
 @section('title', 'Dashboard')
 
 @section('content_header')
-<h1>Dashboard</h1>
+    <h1>Dashboard</h1>
 @stop
 
 @section('content')
 
 
-<x-adminlte-alert theme="success" title="Success">
-  Hey, selamat datang kembali!
-</x-adminlte-alert>
+    <x-adminlte-alert theme="success" title="Success">
+        Hey, selamat datang kembali!
+    </x-adminlte-alert>
 
 
-<div class="row">
-  <div class="col">
-    <div class="info-box">
-      <span class="info-box-icon bg-info"><i class="fa fa-bug"></i></span>
-      <div class="info-box-content">
-        <span class="info-box-text">Jenis Perkara</span>
-        <span class="info-box-number">{{$data['jenis_perkara']}}</span>
-      </div>
+    <div class="row">
+        <div class="col">
+            <div class="info-box">
+                <span class="info-box-icon bg-info"><i class="fa fa-bug"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Jenis Perkara</span>
+                    <span class="info-box-number">{{ $data['jenis_perkara'] }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="info-box bg-success">
+                <span class="info-box-icon"><i class="fa fa-info"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Prasyarat</span>
+                    <span class="info-box-number">{{ $data['prasyarat'] }}</span>
+                </div>
+            </div>
+        </div>
+        <div class="col">
+            <div class="info-box bg-gradient-warning">
+                <span class="info-box-icon"><i class="far fa-copy"></i></span>
+                <div class="info-box-content">
+                    <span class="info-box-text">Perkara</span>
+                    <span class="info-box-number">{{ $data['perkara'] }}</span>
+                </div>
+            </div>
+        </div>
     </div>
-  </div>
-  <div class="col">
-    <div class="info-box bg-success">
-      <span class="info-box-icon"><i class="fa fa-info"></i></span>
-      <div class="info-box-content">
-        <span class="info-box-text">Prasyarat</span>
-        <span class="info-box-number">{{$data['prasyarat']}}</span>
-      </div>
+
+    <div class="card">
+        <div class="card-header">
+            <h3 class="card-title">Grafik Data Perkara</h3>
+            <div class="card-tools">
+                <button type="button" class="btn btn-tool" data-card-widget="collapse">
+                    <i class="fas fa-minus"></i>
+                </button>
+            </div>
+        </div>
+
+        <div class="card-body">
+            <div class="row">
+                <div class="col-sm-4">
+                    <canvas id="myChart"></canvas>
+
+                </div>
+
+
+            </div>
+
+        </div>
+
     </div>
-  </div>
-  <div class="col">
-    <div class="info-box bg-gradient-warning">
-      <span class="info-box-icon"><i class="far fa-copy"></i></span>
-      <div class="info-box-content">
-        <span class="info-box-text">Perkara</span>
-        <span class="info-box-number">{{$data['perkara']}}</span>
-      </div>
-    </div>
-  </div>
-</div>
 @stop
 
 @section('css')
-<link rel="stylesheet" href="/css/admin_custom.css">
+    <link rel="stylesheet" href="/css/admin_custom.css">
 @stop
 
 @section('js')
-<script>
-  setTimeout(() => {
+
+
+    <script src="https://cdn.jsdelivr.net/npm/chart.js"></script>
+
+    <script>
+
+        function randomColor() {
+          var r = Math.floor(Math.random() * 256)
+          var g = Math.floor(Math.random() * 256)
+          var b = Math.floor(Math.random() * 256)
+
+          return `rgb(${r}, ${g}, ${b})`
+        
+        }
+
+        const dataServer =  {!! json_encode($data) !!};;
+        const dataGrafik = dataServer.grafik;
+        var dataSet = []
+        var dataColor = []
+
+        dataGrafik.data.forEach(i => {
+          dataSet.push(i.perkara)
+          dataColor.push(randomColor())
+        });
+
+        const ctx = document.getElementById('myChart');
+
+        const data = {
+            labels: dataGrafik.list_jenis,
+            datasets: [{
+                label: 'Daftar Perkara',
+                data: dataSet,
+                backgroundColor: dataColor,
+                hoverOffset: 4
+            }]
+        };
+
+        const config = {
+            type: 'doughnut',
+            data: data,
+        };
+        new Chart(ctx, config);
+    </script>
+
+
+    <script>
+        setTimeout(() => {
             $(".alert").alert('close');
         }, 4000);
-        
-</script>
+    </script>
 @stop
